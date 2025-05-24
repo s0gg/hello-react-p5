1f7b60385b5e4cda56a2e096c5e12f7ff7dc05aa
s0gg <mail@s0gg.net>
2025-05-24 17:38:28 +0900

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,20 @@
+{
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = { self, nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let
+        pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShells.default = pkgs.mkShell {
+          packages = [
+            pkgs.nodejs_24
+            pkgs.pnpm
+          ];
+        };
+      }
+    );
+}
